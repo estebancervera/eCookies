b0ea5ce56c76ae4525c80d5f9100530a09a1f4bc
Esteban Cervera <cerverae18@hotmail.com>
2020-09-30 12:49:48 -0500

    git ignore attempt 2

--- routes/business/categories.js
+++ routes/business/categories.js
@@ -2,68 +2,67 @@ const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated } = require("../../config/auth");
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 const crypto = require("crypto");
-const fs = require('fs');
-const {promisify } = require('util');
+const fs = require("fs");
+const { promisify } = require("util");
 
-const directoryPath = path.join(__dirname, 'uploads');
+const directoryPath = path.join(__dirname, "uploads");
 const aws = require("aws-sdk");
-const multer = require('multer');
+const multer = require("multer");
 const multerS3 = require("multer-s3");
 
-const S3_BUCKET = "ecookies-assets";
-const AWS_ACCESS_KEY_ID = "AKIATXLCL774X5K6HWWM";
-const AWS_SECRET_ACCESS_KEY = "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";
+const S3_BUCKET = process.env.S3_BUCKET;
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 aws.config.update({
-  secretAccessKey: AWS_SECRET_ACCESS_KEY ,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
   accessKeyId: AWS_ACCESS_KEY_ID,
-  region: 'us-east-1'
+  region: "us-east-1",
 });
 // arn:aws:iam::256293732345:user/Esteban
 const s3 = new aws.S3();
 
+const storage = multerS3({
+  s3: s3,
+  bucket: S3_BUCKET,
+  key: function (req, file, cb) {
+    cb(
+      null,
+      Date.now() +
+        crypto.randomBytes(8).toString("hex") +
+        path.extname(file.originalname)
+    ); //use Date.now() for unique file keys
+  },
+});
 
-
-  const storage = multerS3({
-      s3: s3,
-      bucket: S3_BUCKET,
-      key: function (req, file, cb) {
-          
-          cb(null, Date.now() + crypto.randomBytes(8).toString("hex") + path.extname(file.originalname)); //use Date.now() for unique file keys
-      }
-  });
-
-
-const filefilter = (req, file,cb) => {
-  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+const filefilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
     cb(null, true);
-  }else{
+  } else {
     cb(null, false);
   }
-}
+};
 
 const upload = multer({
   storage: storage,
-  fileFilter: filefilter
+  fileFilter: filefilter,
 });
-
 
 const deleteS3 = function (params) {
   return new Promise((resolve, reject) => {
-      s3.createBucket({
-          Bucket: S3_BUCKET 
-      }, function () {
-          s3.deleteObject(params, function (err, data) {
-              if (err) console.log(err);
-              else
-                  console.log(
-                      "Successfully deleted file from bucket"
-                  );
-              
-          });
-      });
+    s3.createBucket(
+      {
+        Bucket: S3_BUCKET,
+      },
+      function () {
+        s3.deleteObject(params, function (err, data) {
+          if (err) console.log(err);
+          else console.log("Successfully deleted file from bucket");
+        });
+      }
+    );
   });
 };
 
@@ -72,7 +71,7 @@ const Category = require("../../models/category");
 const Business = require("../../models/business");
 
 router.get("/", ensureAuthenticated, function (req, res) {
-    Category.find({business: req.user.business}, function (err, categories) {
+  Category.find({ business: req.user.business }, function (err, categories) {
     if (err) {
       console.log("ERROR: F : " + err);
     } else {
@@ -93,7 +92,6 @@ router.get("/:id/edit", ensureAuthenticated, function (req, res) {
     if (err) {
       res.redirect("/business/categories/");
     } else {
-      
       res.render("business/categories/edit", { category: foundCategory });
     }
   });
@@ -101,7 +99,10 @@ router.get("/:id/edit", ensureAuthenticated, function (req, res) {
 
 //CREATE
 
-router.post("/", ensureAuthenticated, upload.array('file', 1), function (req, res) {
+router.post("/", ensureAuthenticated, upload.array("file", 1), function (
+  req,
+  res
+) {
   var qtyRestricted = req.body.category.qtyRestricted;
 
   if (qtyRestricted === "on") {
@@ -118,7 +119,7 @@ router.post("/", ensureAuthenticated, upload.array('file', 1), function (req, re
     qtyRestricted: req.body.category.qtyRestricted,
     packageSizes: req.body.category.sizes,
     timeRequired: req.body.category.timeRequired,
-    business: req.user.business
+    business: req.user.business,
   });
 
   //req.body.product.description = req.sanitize(req.body.product.description);
@@ -126,67 +127,64 @@ router.post("/", ensureAuthenticated, upload.array('file', 1), function (req, re
   Category.create(category, function (err) {
     if (err) {
       console.log("ERROR: " + err);
-      req.flash("error_msg", "Error al crear la categoria. Intente mas tarde")
-      res.redirect("/business/categories")
-
+      req.flash("error_msg", "Error al crear la categoria. Intente mas tarde");
+      res.redirect("/business/categories");
     } else {
-
-      Business.findOneAndUpdate({ _id: req.user.business}, {$push: {categories: category}}, (err, result) =>{
-        if(err){
+      Business.findOneAndUpdate(
+        { _id: req.user.business },
+        { $push: { categories: category } },
+        (err, result) => {
+          if (err) {
             console.log(err);
-            req.flash("error_msg", `Error al agregar una categoria.`)
+            req.flash("error_msg", `Error al agregar una categoria.`);
             res.redirect("/business/categories");
-        }else{
-          req.flash("success_msg", "Categoria agregada correctamente")
-          res.redirect("/business/categories");
-            
+          } else {
+            req.flash("success_msg", "Categoria agregada correctamente");
+            res.redirect("/business/categories");
+          }
         }
-    });
-     
+      );
     }
   });
 });
 
 //UPDATE ROUTE
 
-router.put("/:id", ensureAuthenticated, upload.array('file', 1), function (req, res) {
-
+router.put("/:id", ensureAuthenticated, upload.array("file", 1), function (
+  req,
+  res
+) {
   var qtyRestricted = req.body.category.qtyRestricted;
 
   //console.log(req.body.category.packageSizes)
 
   if (qtyRestricted === "on") {
-   req.body.category.qtyRestricted = true;
- } else {
-   req.body.category.qtyRestricted = false;
+    req.body.category.qtyRestricted = true;
+  } else {
+    req.body.category.qtyRestricted = false;
   }
   console.log(req.files[0]);
-  if(req.files[0]){
-  Category.findById(req.params.id, (err, foundCategory)=>{
-    if (err){
-      console.log(err);
-      req.flash("error_msg", `Error al editar una categoria.`)
-      res.redirect("/business/categories");
-    }else{
-    const imageFilename = foundCategory.image
-    const params = {
-      Bucket: S3_BUCKET,
-      Key: imageFilename
-    };
+  if (req.files[0]) {
+    Category.findById(req.params.id, (err, foundCategory) => {
+      if (err) {
+        console.log(err);
+        req.flash("error_msg", `Error al editar una categoria.`);
+        res.redirect("/business/categories");
+      } else {
+        const imageFilename = foundCategory.image;
+        const params = {
+          Bucket: S3_BUCKET,
+          Key: imageFilename,
+        };
 
-    deleteS3(params);
+        deleteS3(params);
 
-    foundCategory.image = req.files[0].key;
-  
-    foundCategory.save();
-    
+        foundCategory.image = req.files[0].key;
+
+        foundCategory.save();
+      }
+    });
   }
-
-  });
-}
-
-  
-  
 
   Category.findByIdAndUpdate(req.params.id, req.body.category, function (
     err,
@@ -194,67 +192,48 @@ router.put("/:id", ensureAuthenticated, upload.array('file', 1), function (req, 
   ) {
     if (err) {
       console.log(err);
-      req.flash("error_msg", `Error al agregar una categoria.`)
+      req.flash("error_msg", `Error al agregar una categoria.`);
       res.redirect("/business/categories");
     } else {
-     
-      req.flash("edit_msg", "Categoria editada correctamente")
+      req.flash("edit_msg", "Categoria editada correctamente");
       res.redirect("/business/categories");
     }
   });
-
-
-
 });
 
 //DELETE ROUTE
 
 router.delete("/:id", ensureAuthenticated, function (req, res) {
-
-  Category.findOne({_id: req.params.id}, (err, category) => {
-    if(err){
-      req.flash("error_msg", `Error al eliminar una categoria.`)
+  Category.findOne({ _id: req.params.id }, (err, category) => {
+    if (err) {
+      req.flash("error_msg", `Error al eliminar una categoria.`);
       res.redirect("/business/categories");
-    }else {
-      const imageFilename = category.image
+    } else {
+      const imageFilename = category.image;
       const params = {
         Bucket: S3_BUCKET,
-        Key: imageFilename
+        Key: imageFilename,
       };
 
       deleteS3(params);
     }
-    
-  
 
-    Business.findById(req.user.business, (err, business)=> {
-      if(err){
-        req.flash("error_msg", `Error al eliminar una categoria.`)
-       res.redirect("/business/categories");
-      }else{
+    Business.findById(req.user.business, (err, business) => {
+      if (err) {
+        req.flash("error_msg", `Error al eliminar una categoria.`);
+        res.redirect("/business/categories");
+      } else {
         const index = business.categories.indexOf(category.id);
         business.categories.splice(index, 1);
         business.save();
 
-        
+        category.remove();
 
-         category.remove();
-
-        req.flash("success_msg", "Categoria eliminada correctamente")
+        req.flash("success_msg", "Categoria eliminada correctamente");
         res.redirect("/business/categories");
-
-
-        }
-     
+      }
     });
-
-    
   });
-
-  
 });
-
-
-
 
 module.exports = router;
