427ebb7953821de2660c0c603d73cc7d0a817970
Esteban Cervera <cerverae18@hotmail.com>
2021-01-05 12:50:56 -0600

    added link to coordinates website to edit business

--- models/user.js
+++ models/user.js
@@ -3,53 +3,48 @@ var passportLocalMongoose = require("passport-local-mongoose");
 const Order = require("./order");
 
 var userSchema = new mongoose.Schema({
-    firstname: {
-        type: String,
-        required: true
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  orders: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
     },
-    lastname: {
-        type: String,
-        required: true
-    },
-    email:  {
-        type: String,
-        required: true
-    },
-    password:  {
-        type: String,
-        required: true
-    },
-    orders: [{ 
-        type: mongoose.Schema.Types.ObjectId,
-         ref: "Post"}],
-    phone:  {
-        type: String,
-        required: true
-    },
-    verified: {
-        type: Boolean,
-        default: false
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    banned: {
-        type: Boolean,
-        default: false
-    },
-    bannedBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Business"
-    }
-
-
-
-
+  ],
+  phone: {
+    type: String,
+    required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  banned: {
+    type: Boolean,
+    default: false,
+  },
+  bannedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Business",
+  },
 });
-
-
-
-
 
 module.exports = mongoose.model("User", userSchema);
