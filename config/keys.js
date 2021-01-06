f2f9e867b58871bbcad4f502230b7c4a8a036324
Esteban Cervera <cerverae18@hotmail.com>
2021-01-06 00:33:30 -0600

    added push notification tests 4

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-
-    MongoURI: "mongodb+srv://estebancervera:Cervera2205@ecookies.rqkpz.mongodb.net/ecookies?retryWrites=true&w=majority"
-}
+  MongoURI:
+    "mongodb+srv://estebancervera:diabloPen2205@ecookies.rqkpz.mongodb.net/ecookies?retryWrites=true&w=majority",
+};
