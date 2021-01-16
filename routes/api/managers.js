a7dd8ffe3bbef920941912184f41f080050d5ed9
Esteban Cervera <cerverae18@hotmail.com>
2021-01-16 00:37:23 -0600

    added routes to manager for more control in app

--- routes/api/managers.js
+++ routes/api/managers.js
@@ -8,12 +8,69 @@ var crypto = require("crypto");
 var async = require("async");
 const Order = require("../../models/order");
 const Manager = require("../../models/manager");
+const Business = require("../../models/business");
 
 //Managers API
 router.get("/", authenticateTokenManager, function (req, res) {
   Manager.findById(req.manager.id)
     .populate("business")
-    .then((manager) => res.json({email: manager.email, business: manager.business}))
+    .then((manager) => res.json({ email: manager.email, business: manager.business }))
+    .catch((err) => console.log(err));
+});
+
+router.get("/business/toggle", authenticateTokenManager, function (req, res) {
+  Business.find({ manager: manager }, (err, business) => {
+    if (err) {
+      console.log(err);
+    } else {
+      business.available = !business.available;
+      business.save();
+      res.json({
+        isError: false,
+        message: business.available
+          ? "Se cambio el estado del negocio a disponible"
+          : "Se cambio el estado del negocio a no disponible",
+      });
+    }
+  });
+});
+
+router.get("/business/:lat/:lon", authenticateTokenManager, function (req, res) {
+  Business.find({ manager: manager }, (err, business) => {
+    if (err) {
+      console.log(err);
+    } else {
+      business.lon = req.params.lon;
+      business.lat = req.params.lat;
+      business.save();
+      res.json({
+        isError: false,
+        message: "Se cambio la ubicacion del negocio.",
+      });
+    }
+  });
+});
+
+router.get("/business/:lat/:lon", authenticateTokenManager, function (req, res) {
+  Business.find({ manager: manager }, (err, business) => {
+    if (err) {
+      console.log(err);
+    } else {
+      business.lon = req.params.lon;
+      business.lat = req.params.lat;
+      business.save();
+      res.json({
+        isError: false,
+        message: "Se cambio la ubicacion del negocio.",
+      });
+    }
+  });
+});
+
+router.get("/", authenticateTokenManager, function (req, res) {
+  Manager.findById(req.manager.id)
+    .populate("business")
+    .then((manager) => res.json({ email: manager.email, business: manager.business }))
     .catch((err) => console.log(err));
 });
 
@@ -64,6 +121,56 @@ router.get("/orders", authenticateTokenManager, function (req, res) {
         });
     })
     .catch((err) => console.log(err));
+});
+
+router.get("order/:id/rejected", authenticateTokenManager, async function (req, res) {
+  Order.findById(req.params.id, (err, order) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (order) {
+        if (order.business.equals(req.manager.business)) {
+          if (order.status === "pending") {
+            order.status = "rejected";
+            order.save();
+          }
+        }
+      }
+    }
+  });
+});
+router.get("order/:id/accepted", authenticateTokenManager, function (req, res) {
+  Order.findById(req.params.id, (err, order) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (order) {
+        if (order.business.equals(req.manager.business)) {
+          if (order.status === "pending") {
+            order.status = "accepted";
+            order.save();
+          }
+        }
+      }
+    }
+  });
+});
+
+router.get("order/:id/delivered", authenticateTokenManager, function (req, res) {
+  Order.findById(req.params.id, (err, order) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (order) {
+        if (order.business.equals(req.manager.business)) {
+          if (order.status === "accepted") {
+            order.status = "delivered";
+            order.save();
+          }
+        }
+      }
+    }
+  });
 });
 
 module.exports = router;
