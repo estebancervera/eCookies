641b0244f7f0093185a7269bb54eb53ad8e53e66
Esteban Cervera <cerverae18@hotmail.com>
2021-01-17 21:16:44 -0600

    notifications orders and status and link to manager 10

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -28,7 +28,7 @@ router.get("/", authenticateToken, (req, res) => {
 
 router.post("/", authenticateToken, async (req, res) => {
   var businessId = mongoose.Types.ObjectId(req.body.business);
-  var manager = await Manager.findOne({ business: businessId }).catch((err) => console.log(err));
+  var manager = await Manager.findOne({ business: ObjectId(req.body.business) }).catch((err) => console.log(err));
 
   console.log(manager);
   console.log(req.body.business);
