37b85c4d31bf572e4b424cfa8f18f4a59e520362
estebanC <cerverae18@hotmail.com>
2021-05-26 15:13:41 -0500

    Spacing changes

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,5 @@
-
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-
 
 // Load User model
 const User = require('../models/user');
@@ -9,133 +7,152 @@ const Admin = require('../models/admin');
 const Manager = require('../models/manager');
 
 function SessionConstructor(userId, userGroup, details) {
-    this.userId = userId;
-    this.userGroup = userGroup;
-    this.details = details;
-  }
-
-
-
-module.exports = function(passport){
-    passport.use('local-user-signup', new LocalStrategy({ 
-            usernameField: 'email'}, (email, password, done) => {
-            User.findOne({email: email})
-            .then(user => {
-                if(!user){
-                    return done(null, false, {message: 'That email is not registered'});
-                }
-                
-                bcrypt.compare(password, user.password, (err, isMatch) => {
-                    if (err) throw err;
-                    if(isMatch){
-                        return done(null, user);
-                    }else{
-                        return done(null, false,  {message: 'Contraseña Incorrecta'});
-                    }
-                });
-
-
-            })
-            .catch(err => console.log(err));
-        })
-    );
-
-    passport.use('local-admin-signup', new LocalStrategy({ 
-        usernameField: 'email'}, (email, password, done) => {
-        Admin.findOne({email: email})
-        .then(user => {
-            if(!user){
-                return done(null, false, {message: 'Ese email no esta registrado'});
-            }
-            
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-                if (err) throw err;
-                if(isMatch){
-                    return done(null, user);
-                }else{
-                    return done(null, false,  {message: 'Contraseña Incorrecta'});
-                }
-            });
-
-
-        })
-        .catch(err => console.log(err));
-        })
-    );
-
-    passport.use('local-manager-signup', new LocalStrategy({ 
-        usernameField: 'email'}, (email, password, done) => {
-        Manager.findOne({email: email})
-        .then(user => {
-            if(!user){
-               // req.flash("error_msg", "Ese email no esta registrado")
-                return done(null, false, {message: 'Ese email no esta registrado'});
-            }
-            
-            bcrypt.compare(password, user.password, (err, isMatch) => {
-                if (err) throw err;
-                if(isMatch){
-            
-                    return done(null, user);
-                }else{
-                   // req.flash("error_msg", "Contraseña Incorrecta")
-                    return done(null, false,  {message: 'Contraseña Incorrecta'});
-                }
-            });
-
-
-        })
-        .catch(err => console.log(err));
-        })
-    );
-    
-
-
-    passport.serializeUser((userObject, done) => {
-
-        let userGroup = "admin";
-        let userPrototype = Object.getPrototypeOf(userObject);
-
-        if(userPrototype === Admin.prototype){
-            userGroup = "admin";
-        }else if (userPrototype === User.prototype){
-            userGroup = "user";
-        }else if (userPrototype === Manager.prototype){
-            userGroup = "manager";
-        }
-
-        let sessionConstructor = new SessionConstructor(userObject.id, userGroup, '');
-        done(null, sessionConstructor);
-    });
-
-    passport.deserializeUser((sessionConstructor, done) => {
-
-        if(sessionConstructor.userGroup == "admin"){
-
-            Admin.findOne({
-                _id: sessionConstructor.userId
-            }, '-localStrategy.password', (err, user) => {
-                done(err, user);
-
-            });
-        }else if (sessionConstructor.userGroup == "user"){
-
-            User.findOne({
-                _id: sessionConstructor.userId
-            }, '-localStrategy.password', (err, user) => {
-                done(err, user);
-
-            });
-
-        }else if (sessionConstructor.userGroup == "manager"){
-
-            Manager.findOne({
-                _id: sessionConstructor.userId
-            }, '-localStrategy.password', (err, user) => {
-                done(err, user);
-
-            });
-
-        }
-    });
+	this.userId = userId;
+	this.userGroup = userGroup;
+	this.details = details;
 }
+
+module.exports = function (passport) {
+	passport.use(
+		'local-user-signup',
+		new LocalStrategy(
+			{
+				usernameField: 'email'
+			},
+			(email, password, done) => {
+				User.findOne({ email: email })
+					.then(user => {
+						if (!user) {
+							return done(null, false, {
+								message: 'That email is not registered'
+							});
+						}
+
+						bcrypt.compare(password, user.password, (err, isMatch) => {
+							if (err) throw err;
+							if (isMatch) {
+								return done(null, user);
+							} else {
+								return done(null, false, { message: 'Contraseña Incorrecta' });
+							}
+						});
+					})
+					.catch(err => console.log(err));
+			}
+		)
+	);
+
+	passport.use(
+		'local-admin-signup',
+		new LocalStrategy(
+			{
+				usernameField: 'email'
+			},
+			(email, password, done) => {
+				Admin.findOne({ email: email })
+					.then(user => {
+						if (!user) {
+							return done(null, false, {
+								message: 'Ese email no esta registrado'
+							});
+						}
+
+						bcrypt.compare(password, user.password, (err, isMatch) => {
+							if (err) throw err;
+							if (isMatch) {
+								return done(null, user);
+							} else {
+								return done(null, false, { message: 'Contraseña Incorrecta' });
+							}
+						});
+					})
+					.catch(err => console.log(err));
+			}
+		)
+	);
+
+	passport.use(
+		'local-manager-signup',
+		new LocalStrategy(
+			{
+				usernameField: 'email'
+			},
+			(email, password, done) => {
+				Manager.findOne({ email: email })
+					.then(user => {
+						if (!user) {
+							// req.flash("error_msg", "Ese email no esta registrado")
+							return done(null, false, {
+								message: 'Ese email no esta registrado'
+							});
+						}
+
+						bcrypt.compare(password, user.password, (err, isMatch) => {
+							if (err) throw err;
+							if (isMatch) {
+								return done(null, user);
+							} else {
+								// req.flash("error_msg", "Contraseña Incorrecta")
+								return done(null, false, { message: 'Contraseña Incorrecta' });
+							}
+						});
+					})
+					.catch(err => console.log(err));
+			}
+		)
+	);
+
+	passport.serializeUser((userObject, done) => {
+		let userGroup = 'admin';
+		let userPrototype = Object.getPrototypeOf(userObject);
+
+		if (userPrototype === Admin.prototype) {
+			userGroup = 'admin';
+		} else if (userPrototype === User.prototype) {
+			userGroup = 'user';
+		} else if (userPrototype === Manager.prototype) {
+			userGroup = 'manager';
+		}
+
+		let sessionConstructor = new SessionConstructor(
+			userObject.id,
+			userGroup,
+			''
+		);
+		done(null, sessionConstructor);
+	});
+
+	passport.deserializeUser((sessionConstructor, done) => {
+		if (sessionConstructor.userGroup == 'admin') {
+			Admin.findOne(
+				{
+					_id: sessionConstructor.userId
+				},
+				'-localStrategy.password',
+				(err, user) => {
+					done(err, user);
+				}
+			);
+		} else if (sessionConstructor.userGroup == 'user') {
+			User.findOne(
+				{
+					_id: sessionConstructor.userId
+				},
+				'-localStrategy.password',
+				(err, user) => {
+					done(err, user);
+				}
+			);
+		} else if (sessionConstructor.userGroup == 'manager') {
+			Manager.findOne(
+				{
+					_id: sessionConstructor.userId
+				},
+				'-localStrategy.password',
+				(err, user) => {
+					done(err, user);
+				}
+			);
+		}
+	});
+};
--- models/business.js
+++ models/business.js
@@ -1,51 +1,51 @@
 var mongoose = require("mongoose");
 const Category = require("./category");
 const Manager = require("./manager");
-    
-
 
 var businessSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  categories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
     },
-	image: {
-        type: String,
-        required: true
-    },
-	categories: [{ 
-        type: mongoose.Schema.Types.ObjectId,
-         ref: "Category"}],
-	available:{
-        type: Boolean,
-        required: true
-    },
-    lon:{
-        type: Number,
-        required: true
-    },
-    lat:{
-        type: Number,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    manager: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Manager"
-    }
-
+  ],
+  available: {
+    type: Boolean,
+    required: true,
+  },
+  lon: {
+    type: Number,
+    required: true,
+  },
+  lat: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  manager: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Manager",
+  },
 });
 
-businessSchema.pre('remove', function(next) {
-    // 'this' is the client being removed. Provide callbacks here if you want
-    // to be notified of the calls' result.w
-    
-    Category.remove({business: this._id}).exec();
-    Manager.remove({business: this._id}).exec();
-    next();
+businessSchema.pre("remove", function (next) {
+  // 'this' is the client being removed. Provide callbacks here if you want
+  // to be notified of the calls' result.w
+
+  Category.remove({ business: this._id }).exec();
+  Manager.remove({ business: this._id }).exec();
+  next();
 });
 
 module.exports = mongoose.model("Business", businessSchema);
